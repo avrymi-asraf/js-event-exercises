4b5fc6dbbb58ef43e97e046c1274391bc91a8c37
avizmzm49 <59847752+avizmzm49@users.noreply.github.com>
2021-01-27 11:43:15 +0200

    tooltip

--- chapter 4/tooTip/script.js
+++ chapter 4/tooTip/script.js
@@ -10,11 +10,19 @@ function addAndRemoveSpan(event) {
     tooltipE.classList.add("tooltip");
     tooltipE.innerHTML = buttonE.dataset.tooltip;
     buttonE.appendChild(tooltipE);
+    /// detetmen the location
+    const locButton = buttonE.getBoundingClientRect();
+    const widthTollTip = tooltipE.offsetHeight;
+    console.log(locButton,widthTollTip)
+    // tooltipE.style.transform = "translate(0px, 50%)";
+    if (locButton.top - widthTollTip < 0) {
+      tooltipE.style.transform = "translate(0px, 50%)";
+    }
     document.onmouseout = () => {
       if (tooltipE) tooltipE.remove();
     };
   }
 }
 
-
-console.log(document.querySelector('button').getBoundingClientRect())
+console.log(document.querySelector("button").getBoundingClientRect());
+console.log(document.querySelector("button").offsetHeight);
